03fbe5d557de526599d710da8ea2f444228206ca
jumoog <kilianvonpflugk@gmail.com>
2014-10-27 15:42:52 +0100

    more changes
    Change-Id: Ieea2f38a4d45dd4fe95f1558a5090919d1440ecb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,13 +3,25 @@ include $(CLEAR_VARS)
 
 # Device init files
 
+ifneq ($(STOCK_LAYOUT),false)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_SRC_FILES    := etc/init.qcom.rc.find7u
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif
+
+ifneq ($(STOCK_LAYOUT),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.rc.find7
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
