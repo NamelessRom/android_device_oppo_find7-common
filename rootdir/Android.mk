83d43e95409fc6de68942b23a6431a8440ed9086
Evisceration <eviscerationls@gmail.com>
2014-12-16 21:43:50 +0100

    find7-common: add recovery fstab and init
    Change-Id: I8092cc5f7e709dcfe5ac5baa779f720b58e1ead7

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,3 +54,41 @@ else
   LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
   include $(BUILD_PREBUILT)
 endif
+
+# TWRP
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.find7.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := recovery/init.recovery.find7.rc
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+# Standard
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.twrp.std
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := recovery/fstab.twrp.std
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+# Unified
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.twrp.ufd
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := recovery/fstab.twrp.ufd
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+# Universal layout
+include $(CLEAR_VARS)
+LOCAL_MODULE       := qhdcp.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := recovery/qhdcp.sh
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+include $(BUILD_PREBUILT)
+
